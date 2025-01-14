cd3dc84a725093d69010f68fdebebd798124d031
gabriellemhobson <ghobson@ucsd.edu>
2025-01-14 12:11:32 -0800

    Uncommenting lines so distance field files are created.

--- convert_msh_to_fenics_files.py
+++ convert_msh_to_fenics_files.py
@@ -34,15 +34,15 @@ subprocess.run(command)
 # get the tags associated with the slab interface
 pm = process_mesh.Process_Mesh(T_CG_order,meshfile_name,vizfile_name,v4_vizfile_name,args.mesh_dir)
 # Write to file 
-# X,Y,TAGS = pm.get_slab_interface_tags()
+X,Y,TAGS = pm.get_slab_interface_tags()
 
 # write the transfer matrix that goes from a CG 4 FunctionSpace defined on the mesh to a CG 1 FunctionSpace defined on the viz mesh
 matrix_filename = os.path.join(args.mesh_dir, "M.dat")
-# pm.write_transfer_matrix(matrix_filename)
+pm.write_transfer_matrix(matrix_filename)
 
 # write the x y coord and cell data to file for later plotting
-# pm.write_plotting_data()
+pm.write_plotting_data()
 pm.write_plotting_data_for_compute_mesh()
 
 # compute the distance fields
-# pm.compute_distance_fields(plot_or_not=False)
+pm.compute_distance_fields(plot_or_not=False)
